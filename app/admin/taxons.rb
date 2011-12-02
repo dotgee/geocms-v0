a19f49c655c400fe6948a61a9cbafc069fb9f93d
=atanguy <=atanguy@dotgee.fr>
2011-12-02 20:04:23 +0100

    possibilité d'ordonner les taxons

--- app/admin/taxons.rb
+++ app/admin/taxons.rb
@@ -1,13 +1,40 @@
 ActiveAdmin.register Taxon do
+  controller do
+    
+  def end_of_association_chain
+    return Taxon.order('lft,rgt asc').only_themes
+  end
+
+  end
+
   index do
     column :id
     column :name
-    column "Parent", :parent do |p|
-      link_to p.name, admin_taxon_path(p) unless p.nil?
+    column "Action" do |t|
+      unless t.left_sibling.nil?
+        span link_to "Remonter", move_up_admin_taxon_path(t)
+      end
+      unless t.right_sibling.nil?
+        span link_to "Descendre", move_down_admin_taxon_path(t) 
+      end
     end
-
     default_actions
 
   end
+
+  member_action :move_down do
+    @taxon = Taxon.find(params[:id])
+    @taxon.move_right
+    redirect_to admin_taxons_path
+
+  end
+  member_action :move_up do
+    @taxon = Taxon.find(params[:id])
+    @taxon.move_left
+    redirect_to admin_taxons_path
+
+  end
+
+
   
 end
