02e8c14d9b3d1352c9fff8309f55b8db77fea604
=atanguy <=atanguy@dotgee.fr>
2011-12-16 10:21:59 +0100

    modification de l'affichage des catégories dans l'admin

--- app/admin/taxons.rb
+++ app/admin/taxons.rb
@@ -4,8 +4,7 @@ ActiveAdmin.register Taxon do
     
   def end_of_association_chain
     if action_name == "index"
-      #return Taxon.order('lft,rgt asc').only_themes
-      return Taxon.find('themes').descendants
+      return Taxon.order('lft asc ,rgt asc')
     end
     return super
   end
@@ -22,13 +21,25 @@ ActiveAdmin.register Taxon do
 
   index do
     column :id
-    column :name
-    column "Action" do |t|
-      unless t.left_sibling.nil?
-        span link_to "Remonter", move_up_admin_taxon_path(t)
+    column :name do |t|
+      #div "#{'-' * t.level}#{t.name}"
+      div :class => "tab#{t.level}" do
+        if t.level > 0
+          span t.parent.name
+          span "&rarr;".html_safe
+        end
+
+        b t.name
       end
-      unless t.right_sibling.nil?
-        span link_to "Descendre", move_down_admin_taxon_path(t) 
+    end
+    column "Action" do |t|
+      if !t.root?
+        unless t.left_sibling.nil?
+          span link_to "Remonter", move_up_admin_taxon_path(t)
+        end
+        unless t.right_sibling.nil?
+          span link_to "Descendre", move_down_admin_taxon_path(t) 
+        end
       end
     end
     default_actions
