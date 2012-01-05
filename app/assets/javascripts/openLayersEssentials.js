afa9bd80ad4f24361efe0f4292c301a94a5216a4
=atanguy <=atanguy@dotgee.fr>
2012-01-05 16:14:44 +0100

    modification simpression

--- app/assets/javascripts/openLayersEssentials.js
+++ app/assets/javascripts/openLayersEssentials.js
@@ -1,7 +1,7 @@
 OpenLayers.IMAGE_RELOAD_ATTEMPTS = 2;
 //OpenLayers.ImgPath = "http://js.mapbox.com/theme/dark/"; 
 OpenLayers.ImgPath = "/assets/openlayers/";
-OpenLayers.ProxyHost = "http://geocms.gipbe.dotgee.fr/proxy.php?url=";
+OpenLayers.ProxyHost = "http://geocms.devel.dotgee.fr/proxy.php?url=";
 //var bzhBounds = new OpenLayers.Bounds(33534.34825,6584909.31255,503163.44995,6897995.38035);
 var map, layer, measureControls, mapOptions;
 var format = new OpenLayers.Format.WMC();
