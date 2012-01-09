6b53231404e82a7c18453cb4c9adcaa454ff813e
Jakikiller <antoine.tanguy35@gmail.com>
2012-01-09 12:46:09 +0100

    correction migration

--- db/migrate/20111216094859_add_fiels_to_admin_user.rb
+++
@@ -1,6 +0,0 @@
-class AddFielsToAdminUser < ActiveRecord::Migration
-  def change
-    add_column :admin_users, :last_name, :string
-    add_column :admin_users, :first_name, :string
-  end
-end
--- db/migrate/20120102110411_add_role_id_to_admin_user.rb
+++
@@ -1,5 +0,0 @@
-class AddRoleIdToAdminUser < ActiveRecord::Migration
-  def change
-    add_column :admin_users, :role_id, :integer
-  end
-end
