97a1400a9d0ca68d4b7cd27d44ad73e4485492c3
=atanguy <=atanguy@dotgee.fr>
2012-01-09 15:34:24 +0100

    correction ckeditor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @geo_context = GeoContext.find("default") || GeoContext.last
+    @geo_context = GeoContext.first
     render :template => "geo_contexts/show"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Geocms::Application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
+
   match "sitemap", :controller => :sitemap, :action => :sitemap
 
   get "tag/:id", :as => :tag, :controller => :tag, :action => :show
