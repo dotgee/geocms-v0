4afebff55613c878bd246722b81472205d935805
=atanguy <=atanguy@dotgee.fr>
2012-01-09 14:37:05 +0100

    correciton tag layer published

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -1,7 +1,7 @@
 class TagController < ApplicationController
   def show
     @tag = ActsAsTaggableOn::Tag.find(params[:id])
-    @layers = Layer.tagged_with(@tag).page(page)
+    @layers = Layer.published.tagged_with(@tag).page(page)
   end
 
 end
